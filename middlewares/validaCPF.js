8843ab07661593691596231d8ddb0bcb6e841d45
elvis-brandao <elvis.brandao@outlook.com>
2022-02-19 00:36:55 -0300

    mudança no middleware de validação de cpf

--- middlewares/validaCPF.js
+++ middlewares/validaCPF.js
@@ -1,36 +1,48 @@
-function TestaCPF(strCPF) {
-    var Soma;
-    var Resto;
-    
-    if (strCPF == "00000000000") return false;
-    
-    Soma = 0;
-    for (i=1; i<=9; i++){
-        Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+const CPF = (strCPF) => {
+    return (req, res, next) => {
+        var Soma;
+        var Resto;
+        
+        if (strCPF == "00000000000"){
+            req.cpfValido = false;
+            next();
+        };
+        
+        Soma = 0;
+        for (i=1; i<=9; i++){
+            Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+        };
+        
+        Resto = (Soma * 10) % 11;
+
+        if ((Resto == 10) || (Resto == 11)){
+            Resto = 0;
+        };
+
+        if (Resto != parseInt(strCPF.substring(9, 10))){
+            req.cpfValido = false;
+            next();
+        };
+
+        Soma = 0;
+        for (i = 1; i <= 10; i++){
+            Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+        };
+
+        Resto = (Soma * 10) % 11;
+
+        if ((Resto == 10) || (Resto == 11)){
+            Resto = 0;
+        };
+
+        if (Resto != parseInt(strCPF.substring(10, 11) )){
+            req.cpfValido = false;
+            next();
+        };
+        
+        req.cpfValido = true;
+        next();
     };
-    
-    Resto = (Soma * 10) % 11;
+};
 
-    if ((Resto == 10) || (Resto == 11)){
-        Resto = 0;
-    };
-
-    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
-
-    Soma = 0;
-    for (i = 1; i <= 10; i++){
-        Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
-    };
-
-    Resto = (Soma * 10) % 11;
-
-    if ((Resto == 10) || (Resto == 11)){
-        Resto = 0;
-    };
-
-    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
-    
-    return true;
-}
-
-module.exports = TestaCPF;
+module.exports = CPF;
--- routes/LoginRouters.js
+++ routes/LoginRouters.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var loginController = require('../controllers/LoginControllers');
-//Importar o validador
-const ValidadorDeForm = require('../middlewares/validaCadUsuario');
+//Importando os validadores
+const validadorDeForm = require('../middlewares/validaCadUsuario');
+const validadorCPF = require('../middlewares/validaCPF');
+
 
 /* Routes */
 router.get('/', loginController.login);
-router.post('/cadUsuario', ValidadorDeForm, loginController.cadUsuario);
+router.post('/cadUsuario', validadorDeForm, validadorCPF(), loginController.cadUsuario);
 
 module.exports = router;
