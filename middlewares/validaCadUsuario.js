99c7a86f39f4dc73b616ffb8c46c175cef0ef793
elvis-brandao <elvis.brandao@outlook.com>
2022-02-19 22:56:16 -0300

    Finalmente configurado a validação de cpf no express-validator

--- middlewares/validaCadUsuario.js
+++ middlewares/validaCadUsuario.js
@@ -1,9 +1,7 @@
 const {check, validationResult, body} = require('express-validator');
 const CPF = require('cpf');
 
-const Sequelize = require('sequelize');
 const {Usuario} = require('../models');
-const Op = Sequelize.Op;
 
 let validateRegister = [
     check('nome')
@@ -15,19 +13,21 @@ let validateRegister = [
     check('tel')
         .notEmpty().withMessage('O campo telefone deve ser preenchido').bail()
         .isMobilePhone('pt-BR').withMessage('Por favor usar um telefone válido'),
+    check('dataNasc')
+        .notEmpty().withMessage('Por favor, preencha sua data de nascimento'),
     check('senha')
         .notEmpty().withMessage('Favor definir uma senha').bail()
         .isLength({min: 6}).withMessage('A senha deve ter no mínimo 6 caracteres'),
     body('cpf')
         .custom(cpf => {
             return CPF.isValid(cpf);
-        }).withMessage('O CPF informado é inválido')
+        }).withMessage('O CPF informado é inválido').bail()
         .custom(async cpf => {
             const usuario = await Usuario.findAll({where: {cpf_usuario: cpf}});
-            console.log(usuario.length);
-            console.log(usuario.length > 0 ? false : true)
-            return usuario.length > 0 ? false : true;
-        }).withMessage('O CPF informado já está cadastrado no sistema')
+            if(usuario.length > 0){
+                throw new Error('O CPF informado já está cadastrado no sistema');
+            };
+        })
 ];
 
 module.exports = validateRegister;
